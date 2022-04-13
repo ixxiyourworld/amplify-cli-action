4966b14e37f453ecb6d7b849d35310c3d78b69fc
Jens van Hellemondt <me@jvh.one>
2022-04-13 20:43:15 +0200

    refactor: STACKINFO

--- entrypoint.sh
+++ entrypoint.sh
@@ -53,6 +53,21 @@ which amplify
 echo "amplify version $(amplify --version)"
 
 case $5 in
+  import)
+    echo "# Start initializing Amplify environment: ${ENV}"
+    echo "STACKINFO="`cat $(pwd)team-provider-info.json | json select "$6"`
+
+    echo $STACKINFO
+
+#    echo "# Importing Amplify environment: ${ENV} (amplify env import)"
+#    amplify env import --name ${ENV} --config "${STACKINFO}" --awsInfo ${AWSCONFIG} --yes;
+#    echo "# Initializing existing Amplify environment: ${ENV} (amplify init)"
+#    [[ -z ${CATEGORIES} ]] && amplify init --amplify ${AMPLIFY} --providers ${PROVIDERS} --codegen ${CODEGEN} --yes || amplify init --amplify ${AMPLIFY} --providers ${PROVIDERS} --codegen ${CODEGEN} --categories ${CATEGORIES} --yes
+#    echo "# Environment ${ENV} details:"
+#    amplify env get --name ${ENV}
+#    echo "# Done initializing Amplify environment: ${ENV}"
+    ;;
+
   push)
     amplify push $9 --yes
     ;;
